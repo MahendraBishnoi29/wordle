83c73a2feffdd2b697a1c1005e1335638d9909af
Mahendra <mahendrabrowny@gmail.com>
2023-02-16 19:41:38 +0530

    app context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,128 @@
+import { createContext, useEffect, useState } from "react";
 import "./App.css";
-import Game from "./components/Game";
+import Board from "./components/Board/Board";
+import GameOver from "./components/GameOver/GameOver";
+import Keyboard from "./components/Keyboard/Keyboard";
+import { boardwordle, generateWords } from "./components/Words/Word.jsx";
+
+export const AppContext = createContext();
 
 function App() {
-  const darkHandler = (dark) => {
-    if (dark) document.documentElement.classList.add("dark");
-    else document.documentElement.classList.remove("dark");
+  const [board, setBoard] = useState(boardwordle);
+  const [wordSet, setWordSet] = useState(new Set());
+  const [correctWord, setCorrectWord] = useState("");
+  const [currentAttempt, setCurrAttempt] = useState({
+    attempt: 0,
+    letterPoss: 0,
+  });
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false,
+  });
+
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [almostLetters, setAlmostLetters] = useState([]);
+  const [correctLetters, setCorrectLetters] = useState([]);
+
+  useEffect(() => {
+    generateWords().then((words) => {
+      // console.log(words);
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+  }, []);
+
+  console.log(correctWord);
+
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPoss > 4) return;
+
+    const newBoard = [...board];
+    const currentRow = newBoard[currentAttempt?.attempt];
+    if (currentRow) {
+      currentRow[currentAttempt.letterPoss] = keyVal;
+      setBoard(newBoard);
+      setCurrAttempt({
+        attempt: currentAttempt.attempt,
+        letterPoss: currentAttempt.letterPoss + 1,
+      });
+    }
+  };
+
+  const onEnter = () => {
+    if (currentAttempt.letterPoss !== 5) return;
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currentAttempt.attempt][i];
+    }
+
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currentAttempt.attempt + 1, letterPoss: 0 });
+    }
+    if (!wordSet.has(currWord.toLowerCase())) {
+      alert("Word not found");
+    }
+
+    if (currWord.toLowerCase() === correctWord) {
+      alert("Congratulations, You won!");
+      setGameOver({ gameOver: true, guessedWord: true });
+    }
+
+    if (currentAttempt.attempt > 4) {
+      alert("Game Over");
+      setGameOver({ gameOver: true, guessedWord: true });
+    }
+  };
+
+  const onDelete = () => {
+    if (currentAttempt.letterPoss === 0) return;
+
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPoss - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({
+      ...currentAttempt,
+      letterPoss: currentAttempt.letterPoss - 1,
+    });
   };
 
   return (
-    <div className="app dark:bg-zinc-800">
-      <Game darkness={darkHandler} />
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          correctWord,
+          wordSet,
+          setCorrectWord,
+          currentAttempt,
+          setCurrAttempt,
+          gameOver,
+          setGameOver,
+          disabledLetters,
+          setDisabledLetters,
+          almostLetters,
+          setAlmostLetters,
+          correctLetters,
+          setCorrectLetters,
+          onSelectLetter,
+          onEnter,
+          onDelete,
+        }}
+      >
+        <div className="game">
+          <Board />
+
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
+
+      <br />
+      <br />
     </div>
   );
 }
